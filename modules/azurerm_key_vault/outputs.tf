3e5370342f9cad5b2da7d015d1921eb5296fddce
rajgopal Dey <rajgopaldey.89@gmail.com>
2026-07-23 22:26:26 +0530

    run

--- modules/azurerm_key_vault/outputs.tf
+++ modules/azurerm_key_vault/outputs.tf
@@ -1,18 +1,9 @@
 output "key_vault_id" {
-  description = "Key Vault Resource ID"
   value       = azurerm_key_vault.this.id
-}
-
-output "key_vault_name" {
-  description = "Key Vault Name"
-  value       = azurerm_key_vault.this.name
+  description = "The ID of the Key Vault"
 }
 
 output "key_vault_uri" {
-  description = "Key Vault URI"
   value       = azurerm_key_vault.this.vault_uri
-}
-output "secret_versionless_id" {
-  value       = azurerm_key_vault_secret.db_secret.versionless_id
-  description = "The Versionless ID of the Key Vault Secret"
+  description = "The URI of the Key Vault for environment variables"
 }
