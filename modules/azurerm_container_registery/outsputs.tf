cb5d89e0eb65c1fd28ecf8c9dcbb96524676f2e5
rajgopal Dey <rajgopaldey.89@gmail.com>
2026-07-23 22:15:39 +0530

    run

--- modules/azurerm_container_registery/outsputs.tf
+++ modules/azurerm_container_registery/outsputs.tf
@@ -4,3 +4,9 @@ output "acr_id" {
   value       = azurerm_container_registry.acr.id
   description = "The ID of the Azure Container Registry"
 }
+
+# 🟢 Add this output
+output "login_server" {
+  value       = azurerm_container_registry.acr.login_server
+  description = "The URL that can be used to log into the container registry"
+}
