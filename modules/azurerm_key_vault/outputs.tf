6854ad762d3b9d50580a0b601afd0d985031f577
rajgopal Dey <rajgopaldey.89@gmail.com>
2026-07-23 22:20:01 +0530

    run

--- modules/azurerm_key_vault/outputs.tf
+++ modules/azurerm_key_vault/outputs.tf
@@ -12,3 +12,7 @@ output "key_vault_uri" {
   description = "Key Vault URI"
   value       = azurerm_key_vault.this.vault_uri
 }
+output "secret_versionless_id" {
+  value       = azurerm_key_vault_secret.db_secret.versionless_id
+  description = "The Versionless ID of the Key Vault Secret"
+}
